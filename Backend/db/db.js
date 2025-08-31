9a779c4932e505d8705bb115091de79913b75317
rathoreaditya2302 <adityarathore2302@gmail.com>
2025-08-31 22:16:02 +0530

    Local changes in db.js

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,14 +1,3 @@
-// const mongoose = require('mongoose');
-
-// function connectToDb() {
-//     mongoose.connect(process.env.DB_connect)
-//         .then(() => console.log('Connected to DB'))
-//         .catch(err => console.error(err));
-// }
-
-// module.exports = connectToDb;
-
-
 const mongoose = require('mongoose');
 
 
